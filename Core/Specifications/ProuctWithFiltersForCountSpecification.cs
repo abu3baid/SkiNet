958e2940c0d5b5c2cf067b6885383e5956a73069
Mohammed AbuIbaid <m76eef@gmail.com>
2023-07-18 14:02:03 +0300

    added Pagination

---
+++ Core/Specifications/ProuctWithFiltersForCountSpecification.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Core.Specifications
+{
+    public class ProductsWithFiltersForCountSpecification : BaseSpecification<Product>
+    {
+        public ProductsWithFiltersForCountSpecification(ProductSpecParams productParams) 
+            : base(x =>
+                (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
+                (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
+            )
+        {
+
+        }
+    }
+}
